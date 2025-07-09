648ccd61797a61c3b85960a2eabdc3f765568a00
Bobin <bobinpierreandre@gmail.com>
2025-07-10 00:27:49 +0200

    nav
    mise en place de la nav bar

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,19 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Session;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\DependencyInjection\Loader\Configurator\session;
+
 
 final class SessionController extends AbstractController
 {
     #[Route('/session', name: 'app_session')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {   
         // Render : Permet de faire le lien entre le controller et la vue
+        $sessions = $entityManager->getRepository(Session::class)->findAll(); 
         return $this->render('session/index.html.twig', [
-            'controller_name' => 'SessionController',
+            'sessions' => $sessions
         ]);
     }
 }
---
+++ src/Controller/StagiaireController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\StagiaireRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class StagiaireController extends AbstractController
+{
+    #[Route('/stagiaire', name: 'app_stagiaire')]
+    public function index(StagiaireRepository $stagiaireRepository): Response
+    {
+        
+        $stagiaires = $stagiaireRepository->findAll(); 
+        return $this->render('stagiaire/index.html.twig', [
+            'stagiaires' => $stagiaires
+        ]);
+    }
+}
