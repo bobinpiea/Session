df6b10c7ba002913ecd56df5e72934ac9b937efd
Bobin <bobinpierreandre@gmail.com>
2025-07-14 21:55:53 +0200

    Form Session
    finalisation des label liés au session

--- src/Form/SessionType.php
+++ src/Form/SessionType.php
@@ -4,27 +4,50 @@ namespace App\Form;
 
 use App\Entity\Session;
 use App\Entity\Stagiaire;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class SessionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('placeDisponibles')
-            ->add('dateDebut')
-            ->add('dateFin')
-            ->add('placeRestantes')
-            ->add('placeReservees')
+            ->add('nom', TextType::class, [
+                'label' => 'Nom de la session'
+            ])
+            ->add('placeDisponibles', IntegerType::class, [
+                'label' => 'Places disponibles'
+            ])
+            ->add('dateDebut', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'Date de début'
+            ])
+            ->add('dateFin', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'Date de fin'
+            ])
+            ->add('placeRestantes', IntegerType::class, [
+                'label' => 'Places restantes'
+            ])
+            ->add('placeReservees', IntegerType::class, [
+                'label' => 'Places réservées'
+            ])
             ->add('stagiaires', EntityType::class, [
                 'class' => Stagiaire::class,
-                'choice_label' => 'id',
+                'choice_label' => function($stagiaire) {
+                    return $stagiaire->getNom() . ' ' . $stagiaire->getPrenom();
+                },
+                'label' => 'Stagiaires inscrits',
                 'multiple' => true,
+                'expanded' => true,
             ])
+            ->add('Valider', SubmitType::class)
         ;
     }
 
