9f4220c7d224cfbaae8e4843b200c6830fa98a89
Bobin <bobinpierreandre@gmail.com>
2025-07-14 22:04:11 +0200

    finalisation du formulaire session

--- src/Form/StagiaireType.php
+++ src/Form/StagiaireType.php
@@ -4,30 +4,57 @@ namespace App\Form;
 
 use App\Entity\Session;
 use App\Entity\Stagiaire;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class StagiaireType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('adresse')
-            ->add('ville')
-            ->add('cp')
-            ->add('telephone')
-            ->add('email')
-            ->add('dateNaissance')
+            ->add('nom', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('adresse', TextType::class, [
+                'label' => 'Adresse'
+            ])
+            ->add('ville', TextType::class, [
+                'label' => 'Ville'
+            ])
+            ->add('cp', TextType::class, [
+                'label' => 'Code postal'
+            ])
+            ->add('telephone', TextType::class, [
+                'label' => 'Téléphone'
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse e-mail'
+            ])
+            ->add('dateNaissance', DateType::class, [
+                'label' => 'Date de naissance',
+                'widget' => 'single_text', 
+            ])
             ->add('sessions', EntityType::class, [
                 'class' => Session::class,
-                'choice_label' => 'id',
-                'multiple' => true,
+                'choice_label' => function(Session $session) {
+                    return $session->getNom(); //  on affiche le nom de la session
+                },
+                'label' => 'Sessions inscrites',
+                'multiple' => true, // on peut s'inscrire à plusieurs sessions
+                'expanded' => true 
             ])
-        ;
+            ->add('Valider', SubmitType::class, [
+                'label' => 'Enregistrer le stagiaire'
+            ]);
     }
 
 
